697b363218ac464b212cda28494cb9dca033673e
kehoz <mm@corp.gs>
2020-01-31 15:58:09 -0700

    Update uberAPIController.js

--- controllers/uberAPIController.js
+++ controllers/uberAPIController.js
@@ -1,4 +1,8 @@
-// Main render routine for page data:
+
+  
+var app=angular.module('app', ['atTaskServiceModule']);
+
+
 app.controller('uberAPIController',   function ($scope, $http, $sce, $location, $compile,  atTaskWebService)     
 {
 	alert('test');
