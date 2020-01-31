8926612b025dc1f3b26f5e8877992247716237b6
kehoz <mm@corp.gs>
2020-01-31 16:13:36 -0700

    Update uberAPIController.js

--- controllers/uberAPIController.js
+++ controllers/uberAPIController.js
@@ -2,8 +2,9 @@
   
 var app=angular.module('app', ['atTaskServiceModule']);
 
-
+	alert('test');
+	
 app.controller('uberAPIController',   function ($scope, $http, $sce, $location, $compile,  atTaskWebService)     
 {
-	alert('test');
+
 });
