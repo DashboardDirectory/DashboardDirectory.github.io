2bf1ce4543c0e36b89699372f30d37c60699c25c
Tobias Contreras <tobias.ca@gmail.com>
2024-07-15 20:19:53 -0700

    stopwatch fix toggle only one; remove ' from query string

--- stopwatch/core/js/inject.js
+++ stopwatch/core/js/inject.js
@@ -1,18 +1,15 @@
 function init() {
-    const targetNode = document.getElementById('root'); // or the closest common parent of your React app
+    const targetNode = document.getElementById('root');
     const config = { childList: true, subtree: true };
     var lastTaskNav = '';
     var lastTaskHead = '';
     var getSwHtml = function(name) {
      return '<div role="button" tabindex="0" aria-disabled="false" aria-roledescription="sortable" aria-describedby="DndDescribedBy-7" style="transition: transform 0ms linear 0s;">' +
         '<div data-testid="navitem-tabs-task-stopwatch">' +
-        '<div class="relative "><a class="" style="    background: #ffaa01; display: flex; align-items: center; justify-content: flex-start; height: 32px; white-space: nowrap; text-decoration: none; position: relative; z-index: 2; margin: 0px 8px 4px; padding: 10px;"' +
+        '<div class="relative "><a class="" style="background: #ffaa01; display: flex; align-items: center; justify-content: flex-start; height: 32px; white-space: nowrap; text-decoration: none; position: relative; z-index: 2; margin: 0px 8px 4px; padding: 10px;"' +
         ' href="javascript: (function(){' +
-        'const url = \'https://dashboard.directory/stopwatch/launch.html?autostart=true&taskname='+encodeURIComponent(encodeURIComponent(name))+'\';' +
-        'if (window.swpop && window.swpop.opener) {' +
-        '    window.swpop.location.href = url;' +
-        '    window.swpop.focus();' +
-        '} else {' +
+        'const url = \'https://dashboard.directory/stopwatch/launch.html?autostart=true&taskname='+encodeURIComponent(name.split("'").join(''))+'\';' +
+        'if (window.swpop && window.swpop.opener) { window.swpop.location.href = url; window.swpop.focus(); } else {' +
         'window.swpop = window.open(url,\'swpop\', \'height=600,width=450,left=100,top=100,resizable=yes,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no\');' +
         '}})()"' +
         ' aria-current="page"><span style=" flex-direction: column; display: flex; justify-content: center; margin: 0px 8px 0px 0px; min-width: 20px;" aria-labelledby="Stopwatch">⏱' +
@@ -41,7 +38,6 @@ function init() {
                         let etop = document.createElement('div');
                         etop.innerHTML = getSwHtml(lastTaskHead);
                         shareElement.appendChild(etop);
-                        //etop.previousElementSibling.style = 'margin-right: 12px;';
                     }
                 }
                 break;
