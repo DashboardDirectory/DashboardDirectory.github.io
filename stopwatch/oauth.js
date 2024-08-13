8d200119150dc07cad4217adb808ac555cc41177
Tobias Contreras <tobias.ca@gmail.com>
2024-08-13 08:17:30 -0700

    try to fix one localstorage with multiple sites

--- stopwatch/oauth.js
+++ stopwatch/oauth.js
@@ -13,7 +13,7 @@ const decrypt = (cipherText, key) => {
 };
 var params = new URLSearchParams(document.location.search);
 var clientId = params.get("cid");
-var domain_key = params.get("domain") ?: (localStorage.getItem("domain_key") ?: 'domain_key');
+var domain_key = params.get("domain") ? params.get("domain") : (localStorage.getItem("domain_key") ? localStorage.getItem("domain_key") : 'domain_key');
 
 const setCookie = (cname, cvalue, exdays) => {
     localStorage.setItem(domain_key + '_oauth_'+cname, cvalue);
